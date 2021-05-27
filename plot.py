5962b32049ec353ba717bc6a52c0eb52dab63e28
Peter <peterpaullake@gmail.com>
2021-05-27 15:09:37 -0300

    added thunder bay and windsor

--- plot.py
+++ plot.py
@@ -11,7 +11,7 @@ def plot_data(ax, title, path, bottom=False):
         ax.set_xlabel('Counts per minute')
     ax.set_ylabel('Frequency')
     ax.get_yaxis().set_label_coords(-0.09, 0.5)
-    ax.hist(read_data(path), bins=range(2, 32))
+    ax.hist(read_data(path), bins=range(2, 40))# 32))
 
 def plot():
     data = json.loads(open('json.json').read())
@@ -22,7 +22,7 @@ def plot():
                   d['location_human_readable'],
                   d['path'],
                   bottom=i==n-1)
-    plt.subplots_adjust(hspace=0.5)
-    plt.tight_layout()
+    plt.subplots_adjust(hspace=2)
+    # plt.tight_layout()
     plt.savefig('plot.png', dpi=200)
-    plt.show()
+    # plt.show()
